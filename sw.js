3699790c229f0aca592a03df3f2dc9b7a046452c
yuu <yuuiti.htnk167@gmail.com>
2025-12-05 21:46:10 +0900

    Rename cache and update cached URLs

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = 'truck-fuel-cache-v1';
+const CACHE_NAME = 'fuel-app-cache-v1';
 const URLS_TO_CACHE = [
   './',
   './index.html',
+  './truck.html',
+  './cub.html',
   './manifest.webmanifest'
 ];
 
-self.addEventListener('install', event => { 
+self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
@@ -16,7 +18,9 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
       )
     )
   );
