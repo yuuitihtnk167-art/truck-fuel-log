655d923ff2a2d31dcdc7b454e3ee823840145e19
yuu <yuuiti.htnk167@gmail.com>
2025-12-07 10:32:00 +0900

    Update cub-sw.js

--- cub-sw.js
+++ cub-sw.js
@@ -1,30 +1,32 @@
-const CUB_CACHE_NAME = 'cub-fuel-app-cache-v1';
-const CUB_URLS_TO_CACHE = [
-  './cub.html',
-  './cub-manifest.webmanifest'
+const CACHE_NAME = 'cub-fuel-cache-v1';
+const ASSETS = [
+  '/-/cub.html',
+  '/-/manifest.webmanifest'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
-    caches.open(CUB_CACHE_NAME).then(cache => cache.addAll(CUB_URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(k => k !== CUB_CACHE_NAME)
-          .map(k => caches.delete(k))
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
       )
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  const req = event.request;
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(req).then(cached => {
+      return cached || fetch(req);
+    })
   );
 });
