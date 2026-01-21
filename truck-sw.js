5ebf83ee3bf157364cc30cf450ba594f1e6e8fa3
yuu <yuuiti.htnk167@gmail.com>
2026-01-21 19:54:25 +0900

    Add index redirect for Pages

--- truck-sw.js
+++ truck-sw.js
@@ -1,8 +1,9 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('truck-cache-v4').then((cache) => {
+    caches.open('truck-cache-v5').then((cache) => {
       return cache.addAll([
         './',
+        './index.html',
         './truck.html',
         './truck-manifest.webmanifest',
         './truck-sw.js'
@@ -17,7 +18,7 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((keys) => {
       return Promise.all(
         keys
-          .filter((key) => key !== 'truck-cache-v4')
+          .filter((key) => key !== 'truck-cache-v5')
           .map((key) => caches.delete(key))
       );
     }).then(() => self.clients.claim())
